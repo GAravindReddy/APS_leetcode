cc11913ee402afd113108e4b33648981e738d498
VTU24769 <vtu24769@veltech.edu.in>
2026-02-13 11:03:05 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (18.87%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -0,0 +1,42 @@
+import java.util.Stack;
+
+class MyQueue {
+
+    Stack<Integer> inStack;
+    Stack<Integer> outStack;
+
+    public MyQueue() {
+        inStack = new Stack<>();
+        outStack = new Stack<>();
+    }
+
+    // Push element x to the back of queue
+    public void push(int x) {
+        inStack.push(x);
+    }
+
+    // Removes the element from the front of queue
+    public int pop() {
+        if (outStack.isEmpty()) {
+            while (!inStack.isEmpty()) {
+                outStack.push(inStack.pop());
+            }
+        }
+        return outStack.pop();
+    }
+
+    // Get the front element
+    public int peek() {
+        if (outStack.isEmpty()) {
+            while (!inStack.isEmpty()) {
+                outStack.push(inStack.pop());
+            }
+        }
+        return outStack.peek();
+    }
+
+    // Returns whether the queue is empty
+    public boolean empty() {
+        return inStack.isEmpty() && outStack.isEmpty();
+    }
+}
